a684fff2d2bf16e879d5b251fc6a01d07c6ba09a
henriksih <henrik.sihvonen@gmail.com>
2026-09-03 11:00:08 +0200

    Data seeder created and Progam.cs updated

--- EduSense.API/Program.cs
+++ EduSense.API/Program.cs
@@ -23,6 +23,29 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var appDb = scope.ServiceProvider.GetRequiredService<EduSenseDbContext>();
+    var userDb = scope.ServiceProvider.GetRequiredService<EduSenseUserDbContext>();
+
+    if (app.Environment.IsDevelopment())
+        //Skapa om databasen enligt modellerna
+    {
+        await appDb.Database.EnsureDeletedAsync();
+        await userDb.Database.EnsureDeletedAsync();
+        await appDb.Database.EnsureCreatedAsync();
+        await userDb.Database.EnsureCreatedAsync();
+    }
+    else
+    //Om production mode, kör migrationer som ev inte är körda
+    {
+        await appDb.Database.MigrateAsync();
+        await userDb.Database.MigrateAsync();
+    }
+    //Lägg in seedningsdatat
+    await DataSeeder.SeedAsync(app.Services);
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
