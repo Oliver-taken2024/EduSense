3d2a39e369987c2ed74e3798b29123af3192bf6e
henriksih <henrik.sihvonen@gmail.com>
2026-09-08 11:01:00 +0200

    Added Organisation endpoints for create, update and tests for Controller, Service and Repository

--- EduSense.BLL/Services/IOrganisationService.cs
+++ EduSense.BLL/Services/IOrganisationService.cs
@@ -6,5 +6,8 @@ namespace EduSense.BLL.Services
     public interface IOrganisationService
     {
         Task<IReadOnlyList<OrganisationDto>> GetAllAsync();
+        Task<OrganisationDto?> GetByIdAsync(int id);
+        Task<OrganisationDto> CreateAsync(OrganisationDto organisation);
+        Task<OrganisationDto> UpdateAsync(OrganisationDto organisation);
     }
 }
